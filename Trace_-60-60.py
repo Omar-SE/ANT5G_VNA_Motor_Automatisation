0543f4ab2615fe768820d7e8141ab912a0c6b91e
BOUCHENAK KHELLADI Omar <bouchenak.omar6@gmail.com>
2021-07-25 13:40:47 +0200

    Concaténation d1, d2, d3 (26 ghz)

--- Trace_-60-60.py
+++ Trace_-60-60.py
@@ -14,3 +14,34 @@ import numpy as np
 
 from random import uniform
 from pipython import GCSDevice, pitools
+# récupération data -60 degré
+print('====== LOAD data -60  =======')
+d1 = np.loadtxt('-60.txt')
+print(d1)
+# récupération de l'élement 21 et 22 à 26 GHZ
+print('====== Load 21 -22  =======')
+d1_26ghz = d1[20:22]
+print(d1_26ghz)
+
+# récupération data -50 degré
+print('====== LOAD data -50  =======')
+d2 = np.loadtxt('-50.txt')
+print(d2)
+# récupération de l'élement 21 et 22 à 26 GHZ
+print('====== Load 21 -22  =======')
+d2_26ghz = d2[20:22]
+print(d2_26ghz)
+
+# récupération data -50 degré
+print('====== LOAD data -40  =======')
+d3 = np.loadtxt('-40.txt')
+print(d3)
+# récupération de l'élement 21 et 22 à 26 GHZ
+print('====== Load 21 -22  =======')
+d3_26ghz = d3[20:22]
+print(d3_26ghz)
+
+print('=====BLOCK test ========')
+block = np.hstack((d1_26ghz,d2_26ghz,d3_26ghz))
+print(block)
+np.savetxt('block.txt', block)
