a4d4161e93b2015bf49385c079b30cbbaeca4e28
BOUCHENAK KHELLADI Omar <bouchenak.omar6@gmail.com>
2021-07-25 12:10:57 +0200

    first commit

---
+++ Trace_-60-60.py
@@ -0,0 +1,16 @@
+# Autor : BOUCHENAK KHELLADI OMAR
+
+# Ingénieur système embarqué
+
+# bouchenak.omar6@gmail.com
+
+from RsInstrument.RsInstrument import RsInstrument
+from RsInstrument import *  # The RsInstrument package is hosted on pypi.org, see Readme.txt for more details
+from time import time
+import numpy as np
+from RsInstrument import *  # The RsInstrument package is hosted on pypi.org, see Readme.txt for more details
+from time import time
+import numpy as np
+
+from random import uniform
+from pipython import GCSDevice, pitools
